e5165e927cf0f9bab5cf11cdedae5e8a604a73ce
PARMAR RUTVIK <108228585+Rutvik123456@users.noreply.github.com>
2025-11-04 18:13:37 +0000

    update1

--- mytraJSpractice/index.js
+++ mytraJSpractice/index.js
@@ -1,33 +1,27 @@
 
-let itemarray = 
-    {
-        id: '001',
-        image: 'images/1.jpg',
-        rating: {
-            stars: 4.5,
-            count: 1400,
-        },
-        company: 'Carlton London',
-        item_name: 'Rhodium-Plated CZ Floral Studs',
-        original_price: 1045,
-        current_price: 606,
-    discount_percentage: 42,
-    }   
-        
-let divhtml = document.querySelector('.itemscontainer');
 
-divhtml.innerHTML=` <div class="itemcontainer">
-                <img src="/mytraJSpractice/${itemarray.image} " alt="product-image">
+let insidediv = document.querySelector('.itemscontainer');
+
+
+items.forEach(function(item){
+    insidediv.innerHTML +=  `<div class="itemcontainer">
+                <img src="/mytraJSpractice/${item.image} " alt="product-image">
                 <div class="ratings">
-                    ${itemarray.rating.stars} ⭐ | ${itemarray.rating.count} 
+                    ${item.rating.stars} ⭐ | ${item.rating.count} 
                 </div>
-                <div class="company-name">${itemarray.company}</div>
-                <div class="item-name">${itemarray.item_name}</div>
+                <div class="company-name">${item.company}</div>
+                <div class="item-name">${item.item_name}</div>
                 <div class="pricing">
-                    <span class="current-price">₹${itemarray.current_price}</span>
-                    <span class="original-price">₹${itemarray.original_price}</span>
-                    <span class="discount">(${itemarray.discount_percentage}% OFF)</span>
+                    <span class="current-price">₹${item.current_price}</span>
+                    <span class="original-price">₹${item.original_price}</span>
+                    <span class="discount">(${item.discount_percentage}% OFF)</span>
                 </div>
-                <button class="btn">Add to Bag</button>
+                <button class="btn" onclick="Action();">Add to Bag</button>
 
-            </div>`
+            </div>` 
+})
+
+
+function Action() {
+    
+}
