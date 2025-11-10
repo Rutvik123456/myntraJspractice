af4a14c6dc639df0005861950d65b2ae08437462
PARMAR RUTVIK <108228585+Rutvik123456@users.noreply.github.com>
2025-11-10 16:26:42 +0000

    update3.2

--- mytraJSpractice/pages/bag.js
+++ mytraJSpractice/pages/bag.js
@@ -1,55 +1,67 @@
 
-const item1 = 
-    { 
-        id: '001',
-        image: 'images/1.jpg',
-        company: 'Carlton London',
-        item_name: 'Rhodium-Plated CZ Floral Studs',
-        original_price: 1045,
-        current_price: 606,
-        discount_percentage: 42,
-        return_period: 14,
-        delivery_date: '10 Oct 2023',
-        rating: {
-            stars: 4.5,
-            count: 1400,
-        }}
+// 10/11/25
+
+let bag_container_ele = [1,4,6];
+
+let new_bag_con_ele = bag_container_ele.map(value => {
+  for(let i=0;i<items.length;i++){
+    if(items[i].id == value){
+      return items[i];
+
+    }
+  }
+})
+
+// console.log(new_bag_con_ele);
+
+// ------------------------------------------------------------
+
+
+
+// const item1 = 
+//     { 
+//         id: '001',
+//         image: 'images/1.jpg',
+//         company: 'Carlton London',
+//         item_name: 'Rhodium-Plated CZ Floral Studs',
+//         original_price: 1045,
+//         current_price: 606,
+//         discount_percentage: 42,
+//         return_period: 14,
+//         delivery_date: '10 Oct 2023',
+//         rating: {
+//             stars: 4.5,
+//             count: 1400,
+//         }}
 
 
 loadbic ();
 
 function loadbic (){
     let innerhtml_bag_items_container = document.querySelector('.bag-items-container');
-                innerhtml_bag_items_container.innerHTML = `<div class="bag-item-container">
+                new_bag_con_ele.forEach (){innerhtml_bag_items_container.innerHTML += ( (i) =>{ `<div class="bag-item-container">
 
             <div class="item-left-part">
-              <img class="bag-item-img" src="../${item1.image}">
+              <img class="bag-item-img" src="../${new_bag_con_ele[i].image}">
             </div>
             <div class="item-right-part">
-              <div class="company">${item1.company}</div>
-              <div class="item-name">${item1.item_name}</div>
+              <div class="company">${new_bag_con_ele[i].company}</div>
+              <div class="item-name">${new_bag_con_ele[i].item_name}</div>
               <div class="price-container">
-                <span class="current-price">Rs ${item1.current_price}</span>
-                <span class="original-price">Rs ${item1.original_price}</span>
-                <span class="discount-percentage">(${item1.discount_percentage}% OFF)</span>
+                <span class="current-price">Rs ${new_bag_con_ele[i].current_price}</span>
+                <span class="original-price">Rs ${new_bag_con_ele[i].original_price}</span>
+                <span class="discount-percentage">(${new_bag_con_ele[i].discount_percentage}% OFF)</span>
               </div>
               <div class="return-period">
-                <span class="return-period-days">${item1.return_period} days</span> return available
+                <span class="return-period-days">${new_bag_con_ele[i].return_period} days</span> return available
               </div>
               <div class="delivery-details">
                 Delivery by
-                <span class="delivery-details-days">${item1.delivery_date}</span>
+                <span class="delivery-details-days">${new_bag_con_ele[i].delivery_date}</span>
               </div>
             </div>
 
             <div class="remove-from-cart">X</div>;
           
           </div>`
-                        }  
-
-let bag_container_ele;
-
-let testarr = [1,2,3];
-function filter (array){
-    arr[i]} 
-    filter();
+                        })}}
