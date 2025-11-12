8004d8ce526db58eb45229fdff6162eb0ab01221
PARMAR RUTVIK <108228585+Rutvik123456@users.noreply.github.com>
2025-11-12 13:41:42 +0000

    update4

--- mytraJSpractice/pages/bag.js
+++ mytraJSpractice/pages/bag.js
@@ -1,67 +1,60 @@
 
 // 10/11/25
 
-let bag_container_ele = [1,4,6];
+// for bagitems array from home page
 
-let new_bag_con_ele = bag_container_ele.map(value => {
+ let bag_container_ele = JSON.parse(localStorage.getItem("bagEle"));
+//  let bag_itemss = JSON.parse(bag_container_ele);
+
+
+
+  
+ 
+// load container
+
+let innerhtml_bag_items_container = document.querySelector('.bag-items-container');
+
+function loadbic (){
+                let new_bag_con_ele = bag_container_ele.map(value => {
   for(let i=0;i<items.length;i++){
     if(items[i].id == value){
       return items[i];
 
     }
   }
-})
-
-// console.log(new_bag_con_ele);
-
-// ------------------------------------------------------------
-
-
-
-// const item1 = 
-//     { 
-//         id: '001',
-//         image: 'images/1.jpg',
-//         company: 'Carlton London',
-//         item_name: 'Rhodium-Plated CZ Floral Studs',
-//         original_price: 1045,
-//         current_price: 606,
-//         discount_percentage: 42,
-//         return_period: 14,
-//         delivery_date: '10 Oct 2023',
-//         rating: {
-//             stars: 4.5,
-//             count: 1400,
-//         }}
-
-
-loadbic ();
-
-function loadbic (){
-    let innerhtml_bag_items_container = document.querySelector('.bag-items-container');
-                new_bag_con_ele.forEach (){innerhtml_bag_items_container.innerHTML += ( (i) =>{ `<div class="bag-item-container">
+});
+                new_bag_con_ele.forEach( (i) => {
+                innerhtml_bag_items_container.innerHTML +=  `<div class="bag-item-container">
 
             <div class="item-left-part">
-              <img class="bag-item-img" src="../${new_bag_con_ele[i].image}">
+              <img class="bag-item-img" src="../${i.image}">
             </div>
             <div class="item-right-part">
-              <div class="company">${new_bag_con_ele[i].company}</div>
-              <div class="item-name">${new_bag_con_ele[i].item_name}</div>
+               <div class="company">${i.company}</div>
+              <div class="item-name">${i.item_name}</div>
               <div class="price-container">
-                <span class="current-price">Rs ${new_bag_con_ele[i].current_price}</span>
-                <span class="original-price">Rs ${new_bag_con_ele[i].original_price}</span>
-                <span class="discount-percentage">(${new_bag_con_ele[i].discount_percentage}% OFF)</span>
+                <span class="current-price">Rs ${i.current_price}</span>
+                <span class="original-price">Rs ${i.original_price}</span>
+                <span class="discount-percentage">(${i.discount_percentage}% OFF)</span>
               </div>
               <div class="return-period">
-                <span class="return-period-days">${new_bag_con_ele[i].return_period} days</span> return available
+                <span class="return-period-days">${i.return_period} days</span> return available
               </div>
               <div class="delivery-details">
                 Delivery by
-                <span class="delivery-details-days">${new_bag_con_ele[i].delivery_date}</span>
+                <span class="delivery-details-days">${i.delivery_date}</span>
               </div>
             </div>
 
-            <div class="remove-from-cart">X</div>;
+            <div class="remove-from-cart" onclick="xbut(${i});">X</div>;
           
           </div>`
-                        })}}
+                       } )};
+
+loadbic ();  
+
+function xbut(index) {
+bag_container_ele.splice(index,1);
+innerhtml_bag_items_container.innerHTML='';
+loadbic ();
+}
